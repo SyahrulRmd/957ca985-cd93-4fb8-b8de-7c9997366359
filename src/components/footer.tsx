db962276d41e762812f9b783dab1b6a3b26e2571
SyahrulRmd <87300670+SyahrulRmd@users.noreply.github.com>
2024-02-02 16:22:15 +0700

    add: link github

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,15 @@ const Footer = () => {
       <div className="container py-10">
         <h4 className="text-muted-foreground text-sm mb-4">By Abdurrohim Syahruromadhon Wahyudi</h4>
         <p className="text-muted-foreground text-sm">The source code is available on {' '}
-          <a href='' className="underline hover:opacity-80 transition-all">Github</a>
-          </p>
+          <a 
+            href='https://github.com/SyahrulRmd/957ca985-cd93-4fb8-b8de-7c9997366359' 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:opacity-80 transition-all"
+          >
+            Github
+          </a>
+        </p>
       </div>
     </footer>
   )
